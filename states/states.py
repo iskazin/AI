9e2f0f86f0b5a613da19f882dd45c59500f770f5
iskazin <aibatyr.iskazin@gmail.com>
2026-05-06 12:17:43 +0500

    Update states.py

--- states/states.py
+++ states/states.py
@@ -1,6 +1,5 @@
 from aiogram.fsm.state import State, StatesGroup
 
-
 class OnboardingStates(StatesGroup):
     phase2_complaints = State()
     phase3_history = State()
@@ -8,11 +7,9 @@ class OnboardingStates(StatesGroup):
     phase5_docs = State()
     phase5_result = State()
 
-
 class PaymentStates(StatesGroup):
     waiting_receipt = State()
     waiting_guide_receipt = State()
-
 
 class AdminStates(StatesGroup):
     browsing = State()
