c74187a71d96db9011dfa12c4b07464658acb6d1
iskazin <aibatyr.iskazin@gmail.com>
2026-05-06 12:04:09 +0500

    Add files via upload

---
+++ states/states.py
@@ -0,0 +1,17 @@
+from aiogram.fsm.state import State, StatesGroup
+
+
+class OnboardingStates(StatesGroup):
+    phase2_complaints = State()
+    phase3_history = State()
+    phase4_followup = State()
+    phase5_result = State()
+
+
+class PaymentStates(StatesGroup):
+    waiting_receipt = State()
+    waiting_guide_receipt = State()
+
+
+class AdminStates(StatesGroup):
+    browsing = State()
